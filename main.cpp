332fcd32045208fb8a09db3ccaf11f91efaf90a3
AregAyv <118430155+AregAyv@users.noreply.github.com>
2023-03-25 16:21:36 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,33 +6,38 @@
 #include <sys/user.h>
 #include <ucontext.h>
 #include <pwd.h>
+#include <string.h>
+#include <errno.h>
 
 
+void handle_sigusr1(int sig, siginfo_t *info, void *context) {
+    pid_t sender_pid = info->si_pid;
+    std::cout << "Received a SIGUSR1 signal from process " << sender_pid << " ";
 
-void handle_sigusr1(int sig) {
-    std::cout << "Sender info: << std::endl;
-    
-    pid_t pid = getpid();
-    std::cout << "PID: " << pid << std::endl;
-
-    uid_t uid = getuid();
-    std::cout << "UID: " << uid << std::endl;
-
+  
+    uid_t sender_uid = info->si_uid;
     struct passwd *username = getpwuid(sender_uid);
-    std::cout << "name: " << username->pw_name << std::endl;
+    std::cout << "executed by " << sender_uid << " (" << username->pw_name <<")" <<  std::endl;
 
-    ucontext_t context;
-    getcontext(&context);
-
-    std::cout << "EIP: " << context.uc_mcontext.gregs[REG_RIP] << std::endl;
-    std::cout << "EAX: " << context.uc_mcontext.gregs[REG_RAX] << std::endl;
-    std::cout << "EBX: " << context.uc_mcontext.gregs[REG_RBX] << std::endl;
+   
+    ucontext_t *ucontext = static_cast<ucontext_t *>(context);
+    std::cout<<"State of the context: ";
+    std::cout << "EIP = " << ucontext->uc_mcontext.gregs[REG_RIP] << ", ";
+    std::cout << "EAX = " << ucontext->uc_mcontext.gregs[REG_RAX] << ", ";
+    std::cout << "EBX = " << ucontext->uc_mcontext.gregs[REG_RBX] <<  std::endl;
 }
 
 int main() {
-    pid_t pid = getpid();
-    std::cout << " My PID: " << pid << std::endl;
-    signal(SIGUSR1, handle_sigusr1);
+  
+    struct sigaction action;
+    memset(&action, 0, sizeof(action));
+    action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = handle_sigusr1;
+
+    if (sigaction(SIGUSR1, &action, NULL) < 0) {
+        std::cerr << errno <<  std::endl;
+        exit(errno);
+    }
 
 
     while (true) {
